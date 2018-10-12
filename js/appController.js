cba6055177ae7676762f6261869d217a93bdf58b
Kayode <bkaoluborode@gmail.com>
2018-10-12 18:01:07 +0100

    docs: Add idb database file

--- js/appController.js
+++ js/appController.js
@@ -1,28 +1,80 @@
-/* const fetchGit = ()=> {
-    return fetch('https://api.github.com/repos/Kaytbode/-Server/commits')
-            .then(response => response.json())
-            .then(data => console.log(data.length));
-    }
 
-window.onload = fetchGit; */
 class AppController {
     static gitHubUrl(userName){
         return `https://api.github.com/users/${userName}/repos`;
     }
 
-    static getCommits(userName){
-      const url = AppController.gitHubUrl(userName);
-      let commitsCount = 0;
+    static initializeDB() {
+        const dbPromise = idb.open('commit-step', 2, upgradeDb =>{
+            switch(upgradeDb.oldVersion){
+                case 0:
+                    upgradeDb.createObjectStore('username');
+                case 1:
+                    const store = upgradeDb.createObjectStore('commits');
+                    store.put(0, 'counter');
+            }
+        });
+        return dbPromise;
+    }
 
-      return fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            return data.map(repo => {
-                return fetch(`https://api.github.com/repos/${repo.full_name}/commits`)
-                    .then(response => response.json())
-                    .then(commit => new Promise (resolve=> resolve(commit.length)));
-             })            
+    static addUserToDB(userName) {
+        const dbPromise = AppController.initializeDB();
+
+        dbPromise.then(db => {
+            const tx = db.transaction('username', 'readwrite');
+            tx.objectStore('username').put(userName, 'user');
+            return tx.complete;
         })
-        .catch(err => console.log(err));
+    }
+
+    static getCommitDay() {
+        const dbPromise = AppController.initializeDB();
+        let day;
+
+        dbPromise.then(async db => {
+            const tx = db.transaction('commits', 'readwrite');
+            const store = tx.objectStore('commits');
+            const val = await store.get('counter');
+
+            day = val
+            store.put(val + 1, 'counter');
+            
+            return tx.complete;
+        });
+
+        return day;
+    }
+
+    static async getCommitsCount(userName){
+        const url = AppController.gitHubUrl(userName);
+        let commitsCount = 0;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        return Promise.all(data.map(async repo => {
+            const commitUrl = `https://api.github.com/repos/${repo.full_name}/commits`;
+
+            const res = await fetch(commitUrl);
+            const {length} = await res.json();
+
+            return length;
+        })).then(len => commitsCount += Number(len));
+    }
+
+    static async storeCommitsCount(userName) {
+        const commitsCount = await AppController.getCommitsCount(userName);
+
+        const day = AppController.getCommitDay();
+
+        const dbPromise = AppController.initializeDB();
+
+        dbPromise.then(db => {
+            const tx = db.transaction('commits', 'readwrite');
+            const store = tx.objectStore('commits');
+            store.put(commitsCount, `day ${day}`);
+
+            return tx.complete;
+        });
     }
 }
--- js/events.js
+++ js/events.js
@@ -1,16 +1,19 @@
 
 async function l(x){
-    let a = await AppController.getCommits(x);
+    let a = await AppController.getCommitsCount(x);
 
     console.log(a);
 }
 
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
-    console.log('hey');
     const userName = document.querySelector('input').value;
     console.log(userName);
-    console.log(AppController.getCommits(userName));
+    //console.log(AppController.getCommitsCount(userName));
     l(userName);
+    /* event.preventDefault();
+    fetch('https://api.github.com/users/Kaytbode')
+    .then(res => res.json())
+    .then(val => console.log(val)) */
 });
 
